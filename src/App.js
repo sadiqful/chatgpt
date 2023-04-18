81e1e0fcabb15eda339913c177688df7572c0020
Aliyu <sadiqful@gmail.com>
2023-04-18 10:16:26 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,30 @@
 const App = () => {
+
+  const getMessages = async () => {
+
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: "Hello How are you?"
+      }),
+
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+
+    try {
+
+      const response = await fetch('http://localhost:8000/completions', options);
+      const data = await response.json();
+      console.log(data)
+
+      
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div className="app">
       <section className="side-bar">
@@ -19,7 +45,7 @@ const App = () => {
         <div className="bottom-section">
           <div className="input-container">
           <input/>
-          <div id="submit">➢</div>
+          <div id="submit" onClick={getMessages}>➢</div>
           </div>
           <p className="info">
           ChatGPT Mar 23 Version. Free Research Preview. 
