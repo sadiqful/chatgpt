178f2b199de7631eae812a5b77ee8c0bd6a5260d
Aliyu <sadiqful@gmail.com>
2023-04-24 19:42:21 +0100

    final changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import {useState, useEffect} from 'react'
+
 const App = () => {
 
+  const [message, setMessage] = useState(null)
+  const [value, setValue] = useState(null)
+  const [previousChats, setPreviousChats] = useState([])
+  const [currentTitle, setCurrentTitle] = useState(null)
+
+  const createNewChat = () => {
+    setCurrentTitle(null)
+    setMessage(null)
+    setValue("")
+  }
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle)
+    setMessage(null)
+    setValue("")
+  }
+  
   const getMessages = async () => {
 
     const options = {
@@ -11,7 +30,7 @@ const App = () => {
 
       body: JSON.stringify({
           model: "gpt-3.5-turbo",
-          messages: [{ role: "user", content: "How are you"}],
+          messages: [{ role: "user", content: value}],
           max_tokens: 100,
       })
       
@@ -20,34 +39,67 @@ const App = () => {
 
       const response = await fetch('https://api.openai.com/v1/chat/completions', options);
       const data = await response.json();
-      console.log(data)
-
-      
+      setMessage(data.choices[0].message)     
     } catch (error) {
       console.error(error)
     }
   }
 
+  useEffect(() => {
+    console.log(currentTitle, value, message)
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value)
+    }
+
+    if (currentTitle && value && message) {
+      setPreviousChats(prevChats => (
+        [...prevChats, 
+        {
+          title: currentTitle,
+          role: 'user',
+          content: value
+        },
+
+        {
+          title: currentTitle,
+          role: message.role,
+          content: message.content
+        }
+        
+        ]
+      ))
+    }
+  }, [message, currentTitle])
+
+  console.log(previousChats)
+
+  const currentChats = previousChats.filter(previousChat => previousChat.title === currentTitle)
+  const uniqueTitles = Array.from(new Set(previousChats.map(previousChat => previousChat.title)))
+  console.log(uniqueTitles)
+
   return (
     <div className="app">
       <section className="side-bar">
-        <button>+ New chat</button>
+        <button onClick={createNewChat}>+ New chat</button>
         <ul className="history">
-          <li> History </li>
+          {uniqueTitles?.map((uniqueTitle, index) => <li key={index} onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>)}
         </ul>
         <nav>
           <p>Made by Sadiqful</p>
         </nav>
       </section>
       <section className="main">
-        <h1>ChatGPT</h1>
+       {!currentTitle && <h1>ChatGPT</h1>}
         <ul className="feed">
-
+          {currentChats?.map((chatMessage, index) => <li key={index}>
+            <p className='role'>{chatMessage.role}</p>
+            <p>{chatMessage.content}</p>
+          </li>)}
         </ul>
 
         <div className="bottom-section">
           <div className="input-container">
-          <input/>
+          <input value={value} onChange={(e) => setValue(e.target.value)}/>
           <div id="submit" onClick={getMessages}>➢</div>
           </div>
           <p className="info">
